ea7cbf7e104ac1bf0e814714ef660224c88ca42c
laurapadilla <hi@laurapadilla.xyz>
2022-10-03 16:34:51 -0400

    create context for shopping cart

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import { Routes, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import { Home, Store, About } from './pages';
 import { Navbar } from './components';
+import { ShoppingCartProvider } from './context/ShoppingCartContext';
 
 function App() {
   return (
-    <>
+    <ShoppingCartProvider>
       <Navbar />
       <Container className='mb-4'>
         <Routes>
@@ -14,7 +15,7 @@ function App() {
           <Route path='/about' element={<About />}></Route>
         </Routes>
       </Container>
-    </>
+    </ShoppingCartProvider>
   );
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,44 @@
-import { Container, Navbar as NavbarBS } from 'react-bootstrap';
+import { Button, Container, Nav, Navbar as NavbarBS } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import { FiShoppingCart } from 'react-icons/fi';
+import { IconContext } from 'react-icons';
 
-export const Navbar = () => {
+export function Navbar() {
   return (
-    <NavbarBS className='bg-white shadow-sm bm-3'>
-      <Container>Nav</Container>
+    <NavbarBS sticky='top' className='bg-white shadow-sm bm-3'>
+      <Container>
+        <Nav className='me-auto'>
+          <Nav.Link to={'/'} as={NavLink}>
+            Home
+          </Nav.Link>
+          <Nav.Link to={'/store'} as={NavLink}>
+            Store
+          </Nav.Link>
+          <Nav.Link to={'/about'} as={NavLink}>
+            About
+          </Nav.Link>
+        </Nav>
+        <Button
+          variant='outline-primary'
+          style={{ width: '3rem', height: '3rem', position: 'relative' }}>
+          <IconContext.Provider value={{ size: '1.5rem' }}>
+            <FiShoppingCart />
+          </IconContext.Provider>
+          <div
+            className='rounded-circle bg-danger d-flex justify-content-center align-items-center '
+            style={{
+              color: 'white',
+              width: '1.5rem',
+              height: '1.5rem',
+              position: 'absolute',
+              bottom: '0',
+              right: '0',
+              transform: 'translate(25%, 25%)',
+            }}>
+            3
+          </div>
+        </Button>
+      </Container>
     </NavbarBS>
   );
-};
+}
